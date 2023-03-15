9c859acdae605a15a2dcedd70e37e32400cc3390
Duke4Coco <u531156798@byui.edu>
2023-03-15 14:38:11 +0200

    Modified this code.

--- arrays.js
+++ arrays.js
@@ -1,21 +1,32 @@
-let pieFlavors = 
-                ["Steak and Kidney", "Pepper Steak",
-                "Mutton Curry", "Chicken Mushroom"];
+const print = console.log
 
-for (let i = 0; i < pieFlavors.length; i++) {
-    const pie = pieFlavors[i];
-    console.log(pie)
+let iceCreamFlavors = [ "Chocolate", "Strawberry", "Vanilla", "Pistacio", "Neapolitan", "Mint Chip" ];
+let iceCreamFlavorsObjects = [
+    { name: "Chocolate", type: "Chocolate" }, 
+    { name: "Strawberry", type: "fruit"}, 
+    { name: "Vanilla", type: "Vanilla"}, 
+    { name: "Pistachio", type: "Nuts"}, 
+    { name: "Neapolitan", type: "Chocolate"}, 
+    { name: "Mint Chip", type: "Chocolate"}
+    ];
+
+
+for (let i = 0; i < iceCreamFlavors.length; i++) {
+    const pie = iceCreamFlavors[i];
+    print(pie)
 } // This iterates through the pieFlavors array.
 
 // While this iterates through the indexes of that array.
 let i = 0;
-while (i < pieFlavors.length) {
-    console.log(i)
+while (i < iceCreamFlavors.length) {
+    print(i)
     i++;
 }
 
-pieFlavors.forEach(pie => console.log(pie)) // This loop like the for loop
+iceCreamFlavors.forEach(icecream => print(icecream)) // This loop like the for loop
 // iterates throug the array of pieFlavors and returns each element contained
 // therein.
 
-pieFlavors.forEach((pie, index) => console.log(`Pie Flavor: ${pie}, Index: ${index}`)); 
+iceCreamFlavors.forEach((icecream, index) => print(`Ice Cream: ${icecream}, Index: ${index}`));
+
+iceCreamFlavors.find(flavor => flavor === "Mint Chip"); //returns 'Mint Chip'.
